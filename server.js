f2ef5c65e91533e598baf79cea00e9335a1d0746
dannikaml <dannikaml@hotmail.com>
2023-05-01 12:10:41 -0500

    changed routes to controllers

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
+const routes = require('./controllers');
 const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
-const routes = require('/routes');
 
 const sequelize = require('./config/connection');
 
